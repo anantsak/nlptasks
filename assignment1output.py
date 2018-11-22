9606bd332f4a5b1e66e04b8f0b0e79b4a81dfbf7
anantsak <45262106+anantsak@users.noreply.github.com>
2018-11-22 18:24:07 +0530

    Add files via upload

---
+++ assignment1output.py
@@ -0,0 +1,320 @@
+Python 3.5.4 (v3.5.4:3f56838, Aug  8 2017, 02:17:05) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:\Users\ANANT SAKALLE\Desktop\assignment 1nlp.py =========
+
+Category:  adventure 
+Words:  ['Dan', 'Morgan', 'told', 'himself', 'he', 'would', ...]
+
+Category:  belles_lettres 
+Words:  ['Northern', 'liberals', 'are', 'the', 'chief', ...]
+
+Category:  editorial 
+Words:  ['Assembly', 'session', 'brought', 'much', 'good', ...]
+
+Category:  fiction 
+Words:  ['Thirty-three', 'Scotty', 'did', 'not', 'go', 'back', ...]
+
+Category:  government 
+Words:  ['The', 'Office', 'of', 'Business', 'Economics', '(', ...]
+
+Category:  hobbies 
+Words:  ['Too', 'often', 'a', 'beginning', 'bodybuilder', ...]
+
+Category:  humor 
+Words:  ['It', 'was', 'among', 'these', 'that', 'Hinkle', ...]
+
+Category:  learned 
+Words:  ['1', '.', 'Introduction', 'It', 'has', 'recently', ...]
+
+Category:  lore 
+Words:  ['In', 'American', 'romance', ',', 'almost', 'nothing', ...]
+
+Category:  mystery 
+Words:  ['There', 'were', 'thirty-eight', 'patients', 'on', ...]
+
+Category:  news 
+Words:  ['The', 'Fulton', 'County', 'Grand', 'Jury', 'said', ...]
+
+Category:  religion 
+Words:  ['As', 'a', 'result', ',', 'although', 'we', 'still', ...]
+
+Category:  reviews 
+Words:  ['It', 'is', 'not', 'news', 'that', 'Nathan', ...]
+
+Category:  romance 
+Words:  ['They', 'neither', 'liked', 'nor', 'disliked', 'the', ...]
+
+Category:  science_fiction 
+Words:  ['Now', 'that', 'he', 'knew', 'himself', 'to', 'be', ...]
+
+Category:  acq 
+Words:  ['SUMITOMO', 'BANK', 'AIMS', 'AT', 'QUICK', 'RECOVERY', ...]
+
+Category:  alum 
+Words:  ['SHOWA', 'DENKO', 'EXPORTS', 'ALUMINIUM', 'CASTING', ...]
+
+Category:  barley 
+Words:  ['FRENCH', 'FREE', 'MARKET', 'CEREAL', 'EXPORT', ...]
+
+Category:  bop 
+Words:  ['SOUTH', 'KOREA', 'MOVES', 'TO', 'SLOW', 'GROWTH', ...]
+
+Category:  carcass 
+Words:  ['JAPAN', 'MINISTRY', 'SAYS', 'OPEN', 'FARM', 'TRADE', ...]
+
+Category:  castor-oil 
+Words:  ['INDIA', 'RELAXES', 'RULES', 'FOR', 'EXPORT', ...]
+
+Category:  cocoa 
+Words:  ['COCOA', 'EXPORTERS', 'EXPECTED', 'TO', 'LIMIT', ...]
+
+Category:  coconut 
+Words:  ['PHILIPPINE', 'COCONUT', 'CHIEF', 'TO', 'LOBBY', ...]
+
+Category:  coconut-oil 
+Words:  ['PHILIPPINE', 'COCONUT', 'CHIEF', 'TO', 'LOBBY', ...]
+
+Category:  coffee 
+Words:  ['INDONESIAN', 'COMMODITY', 'EXCHANGE', 'MAY', ...]
+
+Category:  copper 
+Words:  ['BOND', 'CORP', 'STILL', 'CONSIDERING', 'ATLAS', ...]
+
+Category:  copra-cake 
+Words:  ['PHILIPPINE', 'COCONUT', 'CHIEF', 'TO', 'LOBBY', ...]
+
+Category:  corn 
+Words:  ['THAI', 'TRADE', 'DEFICIT', 'WIDENS', 'IN', 'FIRST', ...]
+
+Category:  cotton 
+Words:  ['PAKISTAN', 'COTTON', 'CROP', 'SEEN', 'RECORD', '7', ...]
+
+Category:  cotton-oil 
+Words:  ['NET', 'CHANGE', 'IN', 'EXPORT', 'COMMITMENTS', '--', ...]
+
+Category:  cpi 
+Words:  ['ECONOMIC', 'SPOTLIGHT', '-', 'KUWAITI', 'ECONOMY', ...]
+
+Category:  cpu 
+Words:  ['U', '.', 'S', '.', 'INDUSTRIAL', 'CAPACITY', 'USE', ...]
+
+Category:  crude 
+Words:  ['JAPAN', 'TO', 'REVISE', 'LONG', '-', 'TERM', ...]
+
+Category:  dfl 
+Words:  ['DUTCH', 'OFFICIAL', 'RATE', 'CUT', 'SEEN', 'STILL', ...]
+
+Category:  dlr 
+Words:  ['BANK', 'OF', 'JAPAN', 'INTERVENES', 'SOON', 'AFTER', ...]
+
+Category:  dmk 
+Words:  ['DUTCH', 'OFFICIAL', 'RATE', 'CUT', 'SEEN', 'STILL', ...]
+
+Category:  earn 
+Words:  ['AMATIL', 'PROPOSES', 'TWO', '-', 'FOR', '-', 'FIVE', ...]
+
+Category:  fuel 
+Words:  ['ROYAL', 'DUTCH', '(', 'RD', ')', 'UNIT', 'TO', ...]
+
+Category:  gas 
+Words:  ['FINNS', 'AND', 'CANADIANS', 'TO', 'STUDY', 'MTBE', ...]
+
+Category:  gnp 
+Words:  ['ECONOMIC', 'SPOTLIGHT', '-', 'KUWAITI', 'ECONOMY', ...]
+
+Category:  gold 
+Words:  ['WESTERN', 'MINING', 'TO', 'OPEN', 'NEW', 'GOLD', ...]
+
+Category:  grain 
+Words:  ['CHINA', 'DAILY', 'SAYS', 'VERMIN', 'EAT', '7', '-', ...]
+
+Category:  groundnut 
+Words:  ['JAPAN', "'", 'S', 'LDP', 'URGES', 'MORE', 'IMPORTS', ...]
+
+Category:  groundnut-oil 
+Words:  ['RPT', '-', 'ARGENTINE', 'GRAIN', '/', 'OILSEED', ...]
+
+Category:  heat 
+Words:  ['SUN', '&', 'lt', ';', 'SUN', '>', 'CUTS', 'HEATING', ...]
+
+Category:  hog 
+Words:  ['HOG', 'AND', 'CATTLE', 'SLAUGHTER', 'GUESSTIMATES', ...]
+
+Category:  housing 
+Words:  ['BANK', 'OF', 'AMERICA', 'SEES', 'SLOW', 'U', '.', ...]
+
+Category:  income 
+Words:  ['YUGOSLAV', 'ECONOMY', 'WORSENED', 'IN', '1986', ',', ...]
+
+Category:  instal-debt 
+Words:  ['FED', 'TO', 'RELEASE', 'U', '.', 'S', '.', 'APRIL', ...]
+
+Category:  interest 
+Words:  ['BUNDESBANK', 'ALLOCATES', '6', '.', '1', 'BILLION', ...]
+
+Category:  ipi 
+Words:  ['CHINA', 'INDUSTRIAL', 'OUTPUT', 'RISES', 'IN', ...]
+
+Category:  iron-steel 
+Words:  ['CANADA', 'PLANS', 'TO', 'MONITOR', 'STEEL', ...]
+
+Category:  jet 
+Words:  ['BANGLADESH', 'TENDERS', 'FOR', 'TWO', 'MLN', ...]
+
+Category:  jobs 
+Words:  ['GERMAN', 'INDUSTRIAL', 'EMPLOYMENT', 'SEEN', ...]
+
+Category:  l-cattle 
+Words:  ['MEXICAN', 'CATTLE', 'IMPORTS', 'TO', 'BE', 'BRANDED', ...]
+
+Category:  lead 
+Words:  ['FINNS', 'AND', 'CANADIANS', 'TO', 'STUDY', 'MTBE', ...]
+
+Category:  lei 
+Words:  ['CANADA', 'LEADING', 'INDICATOR', 'UP', '0', '.', '4', ...]
+
+Category:  lin-oil 
+Words:  ['RPT', '-', 'ARGENTINE', 'GRAIN', '/', 'OILSEED', ...]
+
+Category:  livestock 
+Words:  ['JAPAN', 'MINISTRY', 'SAYS', 'OPEN', 'FARM', 'TRADE', ...]
+
+Category:  lumber 
+Words:  ['INDONESIAN', 'COMMODITY', 'EXCHANGE', 'MAY', ...]
+
+Category:  meal-feed 
+Words:  ['RPT', '-', 'ARGENTINE', 'GRAIN', '/', 'OILSEED', ...]
+
+Category:  money-fx 
+Words:  ['BUNDESBANK', 'ALLOCATES', '6', '.', '1', 'BILLION', ...]
+
+Category:  money-supply 
+Words:  ['FED', "'", 'S', 'HELLER', 'SEES', 'RETURN', 'TO', ...]
+
+Category:  naphtha 
+Words:  ['SOUTH', 'KOREA', 'RAISES', 'NAPHTHA', 'PRICE', ...]
+
+Category:  nat-gas 
+Words:  ['JAPAN', 'TO', 'REVISE', 'LONG', '-', 'TERM', ...]
+
+Category:  nickel 
+Words:  ['NICKEL', 'PRICES', 'UNLIKELY', 'TO', 'RISE', 'MUCH', ...]
+
+Category:  nkr 
+Words:  ['NORWAY', 'CENTRAL', 'BANK', 'SELLS', 'CROWNS', 'TO', ...]
+
+Category:  nzdlr 
+Words:  ['NEW', 'ZEALAND', 'BUDGET', 'FORECAST', 'SEEN', 'AS', ...]
+
+Category:  oat 
+Words:  ['NET', 'CHANGE', 'IN', 'EXPORT', 'COMMITMENTS', '--', ...]
+
+Category:  oilseed 
+Words:  ['JAPAN', 'MINISTRY', 'SAYS', 'OPEN', 'FARM', 'TRADE', ...]
+
+Category:  orange 
+Words:  ['WEATHER', 'HURTS', 'ITALIAN', 'ORANGES', '-', 'USDA', ...]
+
+Category:  palladium 
+Words:  ['JOHNSON', 'MATTHEY', "'", 'S', 'PLATINUM', 'GROUP', ...]
+
+Category:  palm-oil 
+Words:  ['INDONESIA', 'SEES', 'CPO', 'PRICE', 'RISING', ...]
+
+Category:  palmkernel 
+Words:  ['US', 'TROPICAL', 'OIL', 'LABELING', 'PLAN', ...]
+
+Category:  pet-chem 
+Words:  ['ENERGY', '/', 'U', '.', 'S', '.', 'PETROCHEMICAL', ...]
+
+Category:  platinum 
+Words:  ['JOHNSON', 'MATTHEY', "'", 'S', 'PLATINUM', 'GROUP', ...]
+
+Category:  potato 
+Words:  ['U', '.', 'K', '.', 'POTATO', 'FUTURES', 'TRADING', ...]
+
+Category:  propane 
+Words:  ['BP', 'CUTS', 'NORTH', 'SEA', 'PROPANE', 'PRICE', ',', ...]
+
+Category:  rand 
+Words:  ['VOLKSKAS', 'BANK', 'SEES', 'RAND', 'HINGING', 'ON', ...]
+
+Category:  rape-oil 
+Words:  ['RPT', '-', 'ARGENTINE', 'GRAIN', '/', 'OILSEED', ...]
+
+Category:  rapeseed 
+Words:  ['JAPAN', 'BUYS', '4', ',', '000', 'TONNES', ...]
+
+Category:  reserves 
+Words:  ['9', '-', 'APR', '-', '1987', '07', ':', '38', ':', ...]
+
+Category:  retail 
+Words:  ['U', '.', 'S', '.', 'MARCH', 'RETAIL', 'SALES', 'CUT', ...]
+
+Category:  rice 
+Words:  ['THAI', 'TRADE', 'DEFICIT', 'WIDENS', 'IN', 'FIRST', ...]
+
+Category:  rubber 
+Words:  ['THAI', 'TRADE', 'DEFICIT', 'WIDENS', 'IN', 'FIRST', ...]
+
+Category:  rye 
+Words:  ['DANISH', 'CROPS', 'TWO', 'WEEKS', 'BEHIND', ',', ...]
+
+Category:  ship 
+Words:  ['AUSTRALIAN', 'FOREIGN', 'SHIP', 'BAN', 'ENDS', 'BUT', ...]
+
+Category:  silver 
+Words:  ['U', '.', 'S', '.', 'DOLLAR', 'LOSSES', 'PROPEL', ...]
+
+Category:  sorghum 
+Words:  ['FURTHER', 'ARGENTINE', 'COARSE', 'GRAIN', 'LOSSES', ...]
+
+Category:  soy-meal 
+Words:  ['U', '.', 'S', '.', 'SUPPLY', '/', 'DEMAND', ...]
+
+Category:  soy-oil 
+Words:  ['MALAYSIA', 'MAY', 'NOT', 'MEET', '1987', 'OIL', ...]
+
+Category:  soybean 
+Words:  ['JAPAN', 'MINISTRY', 'SAYS', 'OPEN', 'FARM', 'TRADE', ...]
+
+Category:  strategic-metal 
+Words:  ['JOHNSON', 'MATTHEY', "'", 'S', 'PLATINUM', 'GROUP', ...]
+
+Category:  sugar 
+Words:  ['THAI', 'TRADE', 'DEFICIT', 'WIDENS', 'IN', 'FIRST', ...]
+
+Category:  sun-meal 
+Words:  ['RPT', '-', 'ARGENTINE', 'GRAIN', '/', 'OILSEED', ...]
+
+Category:  sun-oil 
+Words:  ['RPT', '-', 'ARGENTINE', 'GRAIN', '/', 'OILSEED', ...]
+
+Category:  sunseed 
+Words:  ['FURTHER', 'ARGENTINE', 'COARSE', 'GRAIN', 'LOSSES', ...]
+
+Category:  tea 
+Words:  ['PAKISTAN', 'CONFIRMS', 'KENYA', 'TEA', 'IMPORT', ...]
+
+Category:  tin 
+Words:  ['THAI', 'TRADE', 'DEFICIT', 'WIDENS', 'IN', 'FIRST', ...]
+
+Category:  trade 
+Words:  ['ASIAN', 'EXPORTERS', 'FEAR', 'DAMAGE', 'FROM', 'U', ...]
+
+Category:  veg-oil 
+Words:  ['INDONESIA', 'SEES', 'CPO', 'PRICE', 'RISING', ...]
+
+Category:  wheat 
+Words:  ['SRI', 'LANKA', 'GETS', 'USDA', 'APPROVAL', 'FOR', ...]
+
+Category:  wpi 
+Words:  ['YUGOSLAV', 'ECONOMY', 'WORSENED', 'IN', '1986', ',', ...]
+
+Category:  yen 
+Words:  ['BANK', 'OF', 'JAPAN', 'INTERVENES', 'SOON', 'AFTER', ...]
+
+Category:  zinc 
+Words:  ['THAI', 'ZINC', 'EXPORTS', 'FALL', 'IN', 'MARCH', ...]
+>>> 
---
+++ assignment3output.py
@@ -0,0 +1,32 @@
+Python 3.5.4 (v3.5.4:3f56838, Aug  8 2017, 02:17:05) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:\Users\ANANT SAKALLE\Desktop\assignment 3.py ==========
+('abdication', ['AE2', 'B', 'D', 'IH0', 'K', 'EY1', 'SH', 'AH0', 'N'])
+('abdnor', ['AE1', 'B', 'D', 'N', 'ER0'])
+('abdo', ['AE1', 'B', 'D', 'OW0'])
+('abdollah', ['AE2', 'B', 'D', 'AA1', 'L', 'AH0'])
+('abdomen', ['AE0', 'B', 'D', 'OW1', 'M', 'AH0', 'N'])
+('abdomen', ['AE1', 'B', 'D', 'AH0', 'M', 'AH0', 'N'])
+('abdominal', ['AE0', 'B', 'D', 'AA1', 'M', 'AH0', 'N', 'AH0', 'L'])
+('abdominal', ['AH0', 'B', 'D', 'AA1', 'M', 'AH0', 'N', 'AH0', 'L'])
+('abduct', ['AE0', 'B', 'D', 'AH1', 'K', 'T'])
+('abducted', ['AE0', 'B', 'D', 'AH1', 'K', 'T', 'IH0', 'D'])
+('abducted', ['AH0', 'B', 'D', 'AH1', 'K', 'T', 'IH0', 'D'])
+('abductee', ['AE0', 'B', 'D', 'AH2', 'K', 'T', 'IY1'])
+('abductees', ['AE0', 'B', 'D', 'AH2', 'K', 'T', 'IY1', 'Z'])
+('abducting', ['AE0', 'B', 'D', 'AH1', 'K', 'T', 'IH0', 'NG'])
+('abducting', ['AH0', 'B', 'D', 'AH1', 'K', 'T', 'IH0', 'NG'])
+('abduction', ['AE0', 'B', 'D', 'AH1', 'K', 'SH', 'AH0', 'N'])
+('abduction', ['AH0', 'B', 'D', 'AH1', 'K', 'SH', 'AH0', 'N'])
+('abductions', ['AE0', 'B', 'D', 'AH1', 'K', 'SH', 'AH0', 'N', 'Z'])
+('abductions', ['AH0', 'B', 'D', 'AH1', 'K', 'SH', 'AH0', 'N', 'Z'])
+('abductor', ['AE0', 'B', 'D', 'AH1', 'K', 'T', 'ER0'])
+('abductor', ['AH0', 'B', 'D', 'AH1', 'K', 'T', 'ER0'])
+('abductors', ['AE0', 'B', 'D', 'AH1', 'K', 'T', 'ER0', 'Z'])
+('abductors', ['AH0', 'B', 'D', 'AH1', 'K', 'T', 'ER0', 'Z'])
+('abducts', ['AE0', 'B', 'D', 'AH1', 'K', 'T', 'S'])
+('abdul', ['AE0', 'B', 'D', 'UW1', 'L'])
+[('Hello', 'NNP'), ('i', 'NN'), ('study', 'VBP'), ('computer', 'NN'), ('science', 'NN'), ('and', 'CC'), ('my', 'PRP$'), ('name', 'NN'), ('is', 'VBZ'), ('anant', 'JJ'), ('sakalle', 'NN')]
+['Hello', 'people', '!', 'I', 'love', 'to', 'play', 'football', 'and', 'listen', 'to', 'music', '.', 'I', 'also', 'love', 'spicy', 'food']
+>>> 
